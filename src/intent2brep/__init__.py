c8a264e23558312a2fbc0565775ad2299c7d041d
0Pursuer <140836050+0Pursuer@users.noreply.github.com>
2026-09-02 17:29:51 +0800

    refactor: remove parametric text-to-JSON public API

--- src/intent2brep/__init__.py
+++ src/intent2brep/__init__.py
@@ -1,7 +1,18 @@
-from .models import PartIntent
-from .pipeline import PipelineResult, run_pipeline, run_parametric_pipeline
-from .pipelines.visual import VisualPipelineResult, run_image_to_mesh, run_text_to_image, run_text_to_mesh, run_views_to_mesh
+from .pipelines.visual import (
+    VisualPipelineResult,
+    run_image_to_mesh,
+    run_text_to_image,
+    run_text_to_mesh,
+    run_views_to_mesh,
+)
 
-__version__ = "0.4.0"
-__all__ = ["PartIntent", "PipelineResult", "run_pipeline", "run_parametric_pipeline", "VisualPipelineResult",
-           "run_text_to_image", "run_image_to_mesh", "run_text_to_mesh", "run_views_to_mesh", "__version__"]
+__version__ = "0.5.0"
+
+__all__ = [
+    "VisualPipelineResult",
+    "run_text_to_image",
+    "run_image_to_mesh",
+    "run_text_to_mesh",
+    "run_views_to_mesh",
+    "__version__",
+]
